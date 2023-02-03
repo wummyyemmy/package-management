c100a3d164ad8d8cd9c795593c0d97f66101b914
wummyyemmy <113473594+wummyyemmy@users.noreply.github.com>
2023-02-03 03:59:07 +0100

    Update worker-k8s.sh

--- kubeadm/worker-k8s.sh
+++ kubeadm/worker-k8s.sh
@@ -99,6 +99,5 @@ systemctl enable kubelet.service
 #Copy kubeadm join token from the master and execute in Worker Nodes to join to cluster
 # replace this token with yours
 
-kubeadm join 10.0.0.11:6443 --token 03em5o.agjpy9wbj98izbpn \
-        --discovery-token-ca-cert-hash sha256:92a1cf1e452961a550f05eff32f25500fc9bcfd5d5f771689e8ee21b6a0da243
-
+kubeadm join 172.31.23.141:6443 --token 8bdcde.ap5ec1iriblwaib9 \
+        --discovery-token-ca-cert-hash sha256:abff5124e335b973b3a910573afe75b24808dcbc0a06574c6fe6991fdd3d0c2b
